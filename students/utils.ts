6396bbffb8b60236747dfbc8ae03669f50d38340
tahott <tahott@gmail.com>
2021-04-25 20:09:49 +0900

    students API 개발
    * added. test code
    * added. utils/allowedKorean, utils/birthValid
    * added. method get/post func split

---
+++ students/utils.ts
@@ -0,0 +1,15 @@
+export const allowedKorean = (str: string): boolean => {
+  const hangulFilter = /(?:[^\uAC00-\uD7A3\u3131-\u314E\u314F-\u3163\u318D\u119E\u11A2\u2022\u2025\u00B7\uFE55]|_)+/g
+
+  return !hangulFilter.test(str) && str.length > 1 && str.length < 11
+}
+
+export const birthValid = (str: string): boolean => {
+  const [year, month, day] = str.split('-')
+
+  const yearValid = Number(year) > 1900 && Number(year) < 2050 && year.length === 4
+  const monthValid = Number(month) > 0 && Number(month) < 13 && month.length === 2
+  const dayValid = Number(day) > 0 && Number(day) < 32 && day.length === 2
+
+  return yearValid && monthValid && dayValid && str.length === 10
+}
