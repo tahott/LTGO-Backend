f3d18b233bdc230a766dd67f1cda71d3d53a617f
tahott <41527763+tahott@users.noreply.github.com>
2021-05-02 11:34:06 +0900

    Feat/patch student (#3)
    * 원생 삭제 API
    
    added. delete student
    
    * student 수정 API
    
    added. [patch] student update api
    added. idPatternCheck func.
    added. test code
    modified. return status code

--- student/index.ts
+++ student/index.ts
@@ -1,38 +1,94 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
-import { CosmosClient, Item } from '@azure/cosmos'
+import { CosmosClient } from '@azure/cosmos'
+
+import { idPatternCheck } from './utils'
 
 const client = new CosmosClient({ endpoint: process.env["DbEndPoint"], key: process.env["DbKey"] });
 
 const methodGet = async function (context: Context, req: HttpRequest) {
   const { id } = req.params;
+
+  if (!idPatternCheck(id)) {
+    context.res = {
+      status: 400,
+      body: 'Bad Request',
+    }
+    context.done();
+  }
+
   const container = await client.database('ltgo').container('students');
-  const { resources } = await container.items.query(`
-    select
-      c["name"],
-      c["birth"],
-      c["group"]
-    from c
-    where c["id"] = "${id}"
-  `).fetchAll()
+  const { resource } = await container.item(id, id).read()
+
+  if (!resource) {
+    context.res = {
+      status: 404,
+      body: 'Not Found',
+    }
+    context.done();
+  }
 
   context.res = {
     status: 200,
-    body: resources
+    body: {
+      name: resource.name,
+      birth: resource.birth,
+      group: resource.group,
+    }
   }
 }
 
 const methodPatch = async function (context: Context, req: HttpRequest) {
+  const { id } = req.params;
+  const { group: g } = req.body;
 
+  if (!g || !idPatternCheck(id)) {
+    context.res = {
+      status: 400,
+      body: 'Bad Request',
+    }
+    context.done();
+  }
+
+  const container = await client.database('ltgo').container('students');
+  const { resource: res } = await container.item(id, id).read();
+
+  if (!res) {
+    context.res = {
+      status: 404,
+      body: 'Not Found',
+    }
+    context.done();
+  }
+
+  const { resource } = await container.items.upsert(Object.assign(res, { group: g }))
+
+  const { name, birth, group } = resource;
+
+  context.res = {
+    body: {
+      name,
+      birth,
+      group,
+    }
+  }
 }
 
 const methodDelete = async function (context: Context, req: HttpRequest) {
   const { id } = req.params;
+
+  if (!idPatternCheck(id)) {
+    context.res = {
+      status: 400,
+      body: 'Bad Request',
+    }
+    context.done();
+  }
+
   const container = await client.database('ltgo').container('students');
+  const item = await container.item(id, id);
+  await item.delete();
 
-  const item = await container.item(id, id)
-  await item.delete()
-
-  context.done()
+  context.done();
 }
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
